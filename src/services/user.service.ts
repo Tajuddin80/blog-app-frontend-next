e4bc002401da9f0ab3cc73908532251a7e6276d6
Tajuddin80 <tajuddin.cse.dev@gmail.com>
2026-08-11 22:58:36 +0600

    feat: implement user session service with env validation and configuration

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,30 +1,31 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
-const AUTH_URL = process.env.AUTH_URL
+
+const AUTH_URL = env.AUTH_URL;
 
 export const userService = {
-    getSession: async function () {
-        try {
-            const cookieStore = await cookies();
+  getSession: async function () {
+    try {
+      const cookieStore = await cookies();
 
-            // console.log(cookieStore.get("better-auth.session-token"));
+      console.log(cookieStore.toString());
 
-            const res = await fetch(`${AUTH_URL}/get-session`, {
-                headers: {
-                    Cookie: cookieStore.toString(),
-                },
-                cache: "no-store",
-            });
+      const res = await fetch(`${AUTH_URL}/get-session`, {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+        cache: "no-store",
+      });
 
-            const session = await res.json()
+      const session = await res.json();
 
-            if (session === null) {
-                return { data: null, error: "No session found" }
-            }
+      if (session === null) {
+        return { data: null, error: "No session found" };
+      }
 
-            return { data: session, error: null }
-
-        } catch (err) {
-            return { data: null, error: err }
-        }
+      return { data: session, error: null };
+    } catch (err) {
+      return { data: null, error: err };
     }
-}
+  },
+};
